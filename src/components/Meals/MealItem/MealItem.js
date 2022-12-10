d1350440e5b4753b205697a72b8ebb867f833955
timurmalkoc <malkoctimurcelik@gmail.com>
2022-12-10 18:04:43 -0500

    adding picture

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -3,6 +3,17 @@ import { useContext } from "react";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
 import CartContext from "../../../store/cart-context";
+import Sushi from "../../../assets/sushi.jpg";
+import Schnitzel from "../../../assets/Schnitzel.jpg";
+import BarbecueBurger from "../../../assets/Barbecue-Burger.jpg";
+import GreenBowl from "../../../assets/Green-Bowl.jpg";
+
+const images = {
+    Sushi,
+    Schnitzel,
+    "Barbecue Burger": BarbecueBurger,
+    "Green Bowl": GreenBowl
+}
 
 const MealItem = props => {
     const cartCtx = useContext(CartContext);
@@ -19,14 +30,20 @@ const MealItem = props => {
     };
 
     return (
-    <li className={classes.meal}>
+        
+    <li className={classes.card}>
         <div>
-            <h3>{props.name}</h3>
-            <div className={classes.description}>{props.description}</div>
-            <div className={classes.price}>{price}</div>
+            <img src={images[props.name]} />
         </div>
-        <div>
-            <MealItemForm id={props.id} onAddToCart={addItemToCartHandler}/>
+        <div className={classes.meal}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={classes.description}>{props.description}</div>
+                <div className={classes.price}>{price}</div>
+            </div>
+            <div>
+                <MealItemForm id={props.id} onAddToCart={addItemToCartHandler}/>
+            </div>
         </div>
     </li>
     );
